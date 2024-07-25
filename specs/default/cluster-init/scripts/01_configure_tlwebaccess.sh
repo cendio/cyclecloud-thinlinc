60d9d2ce7fbcaf374d9acf158cc2a9a8c100f7cc
Xuan <xuan@dhcp-130.guest.cendio.se>
2024-07-25 16:19:31 +0200

    update configuration scripts

--- specs/default/cluster-init/scripts/01_configure_tlwebaccess.sh
+++ specs/default/cluster-init/scripts/01_configure_tlwebaccess.sh
@@ -1,43 +1,39 @@
 #!/bin/bash
 
-# Define the path to the configuration file
-CONFIG_FILE="/opt/thinlinc/etc/conf.d/webaccess.hconf"
-
 # Fetch thinlinc web access parameters
 enable_web=$(jetpack config thinlinc.enable_web False)
 thinlinc_web_port=$(jetpack config thinlinc.web_port 443)
 
 # Function to restart tlwebaccess service
 restart_tlwebaccess() {
-    if ! systemctl restart tlwebaccess; then
-        echo "Failed to restart tlwebaccess service."
-        exit 1
-    fi
-    echo "Thinlinc tlwebaccess service restarted successfully."
+	if ! systemctl restart tlwebaccess; then
+	    echo "Failed to restart tlwebaccess service."
+	    exit 1
+	fi
+	echo "Thinlinc tlwebaccess service restarted successfully."
 }
 
 # Function to disable tlwebaccess service
 disable_tlwebaccess() {
-    if ! systemctl stop tlwebaccess; then
-        echo "Failed to stop tlwebaccess service."
-        exit 1
-    fi
-    echo "Thinlinc tlwebaccess service stopped successfully."
+  if ! systemctl disable --now tlwebaccess; then
+      echo "Failed to disable tlwebaccess service."
+      exit 1
+  fi
+  echo "Thinlinc tlwebaccess service disable successfully."
 }
 
-# Update the /opt/thinlinc/etc/conf.d/webaccess.hconf file
 if [[ "$enable_web" == "True" ]]; then
-    if [[ -f "$CONFIG_FILE" ]]; then
-      sed -i "s/^listen_port=.*/listen_port=$thinlinc_web_port/" $CONFIG_FILE
-    else
-      echo "Configuration file not found: $CONFIG_FILE"
-      exit 1
-    fi
-    # Restart the tlwebaccess service to apply changes
-    restart_tlwebaccess
-    echo "Thinlinc Web Access port number configurations completed."
+	# Update the listen_port
+	/opt/thinlinc/bin/tl-config /webaccess/listen_port=$thinlinc_web_port
+	if [[ $? -ne 0 ]]; then
+	  echo "Failed to configure Thinlinc Web Access port number."
+	  exit 1
+	fi
+	# Restart the tlwebaccess service to apply changes
+	restart_tlwebaccess
+	echo "Thinlinc Web Access port number configurations completed."
 else
-    # Disable the tlwebaccess service
-    disable_tlwebaccess
-    echo "Disable Thinlinc Web access completed."
+	# Disable the tlwebaccess service
+	disable_tlwebaccess
+	echo "Disable Thinlinc Web access completed."
 fi
--- specs/default/cluster-init/scripts/02_configure_vsmagent.sh
+++ specs/default/cluster-init/scripts/02_configure_vsmagent.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-# Define the path to the configuration file
-CONFIG_FILE="/opt/thinlinc/etc/conf.d/vsmagent.hconf"
-
 # Fetch the connection mode 
 connection_mode=$(jetpack config thinlinc.connection_mode private_ip)
 
 # Determine the agent_hostname based on the connection mode
 case "$connection_mode" in
     "private_ip")
-        thinlinc_agent_hostname=""
+        thinlinc_agent_hostname=$(jetpack config cloud.local_ipv4)
         ;;
     "public_ip")
         thinlinc_agent_hostname=$(jetpack config cloud.public_ipv4)
@@ -23,11 +20,10 @@ case "$connection_mode" in
         ;;
 esac
 
-# Update the /opt/thinlinc/etc/conf.d/vsmagent.hconf file
-if [[ -f "$CONFIG_FILE" ]]; then
-    sed -i "s/^agent_hostname=.*/agent_hostname=$thinlinc_agent_hostname/" "$CONFIG_FILE"
-else
-  echo "Configuration file not found: $CONF_FILE"
+# Update the agent_hostname
+/opt/thinlinc/bin/tl-config /vsmagent/agent_hostname=$thinlinc_agent_hostname
+if [[ $? -ne 0 ]]; then
+  echo "Failed to configure Thinlinc agent_hostname."
   exit 1
 fi
 
